d1884388668cd6986fe565e2c01b6df6aec61d55
Brais P. Castro <braispcastro@gmail.com>
2020-06-07 03:08:37 +0200

    Project created

---
+++ app/src/main/java/com/braispc/sunstatus/ui/views/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.braispc.sunstatus.ui.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.braispc.sunstatus.R
+import com.braispc.sunstatus.common.Constants
+import com.squareup.picasso.MemoryPolicy
+import com.squareup.picasso.NetworkPolicy
+import com.squareup.picasso.Picasso
+
+lateinit var imgSun: ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imgSun = findViewById<ImageView>(R.id.imgSun)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Picasso.get()
+            .load(Constants.SUN_URL)
+            .networkPolicy(NetworkPolicy.NO_CACHE)
+            .memoryPolicy(MemoryPolicy.NO_CACHE)
+            .centerInside()
+            .fit()
+            .into(imgSun)
+    }
+}
